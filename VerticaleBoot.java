cce3e2c05aa837186bd3d82687c9ef223cbd87b5
Pieter Herinckx <pieterherinckx@katienen.lan>
2019-03-14 14:16:07 +0100

    Update VerticaleBoot.java

--- VerticaleBoot.java
+++ VerticaleBoot.java
@@ -1,38 +1,49 @@
-public class VerticaleBoot implements Boot {
-    private int x;
-    private int y;
+
+/**
+ * Write a description of class VerticaleBoot here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class VerticaleBoot implements Boot
+{
+    private int positieX;
+    private int positieY;
     private int grootte;
-    
-    public VerticaleBoot(int x, int y, int grootte){
-        this.x = x;
-        this.y = y;
+
+    public VerticaleBoot(int positieX, int positieY, int grootte){
+        this.positieX = positieX;        
+        this.positieY = positieY;
         this.grootte = grootte;
     }
-    
-    public void toonBoot(){      
-        for (int i = 0; i<grootte; i++){
-            System.out.println(x + "," + (y+i));
-        }
-    }
-    
-    public boolean isRaak(int raakX, int raakY){
-        boolean raak = false;   
-        for (int i = 0; i<grootte; i++){
-            if((raakX == x) && (raakY == (y+i))){
-                raak = true;
-            }
-        }        
-        return raak;
-    }
-    
-    public boolean overlaptBoot(Boot testboot){
-        boolean overlapt = false;
-        for (int i = 0; i<grootte; i++){
-            if(testboot.isRaak(x,(y+i))){
-                overlapt = true;
-            }
-        }        
-        return overlapt;
+
+    public VerticaleBoot(){
+        this(6,5,4);
     }
 
+    public void toonBoot(){
+        for(int i=0;i<grootte;i++){  
+            System.out.println(positieX+","+(positieY+i)); 
+        }
+    }
+
+    public boolean isRaak(int x, int y){
+        boolean raak = false;
+        for(int i=0;i<grootte;i++){
+            if((x == positieX) && (y == (positieY+i))){
+                raak = true;
+            } 
+        }
+        return raak;
+    } 
+
+    public boolean overlaptBoot(Boot testboot){
+        boolean overlapt = false;
+        for(int i=0;i<grootte;i++){
+            if(testboot.isRaak(positieX,(positieY+i))){
+                overlapt = true;
+            }            
+        }
+        return overlapt;
+    }
 }
