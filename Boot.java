b2fbac990487b7d9ce8919ed60f3c92cd601a82b
Pieter Herinckx <pieterherinckx@katienen.lan>
2019-03-14 14:15:42 +0100

    Update Boot.java

--- Boot.java
+++ Boot.java
@@ -1,5 +1,13 @@
-public interface Boot {
+
+/**
+ * Write a description of interface Boot here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public interface Boot
+{
     public void toonBoot();
-    public boolean isRaak(int raakX, int raakY);
+    public boolean isRaak(int x, int y);
     public boolean overlaptBoot(Boot testboot);
 }
